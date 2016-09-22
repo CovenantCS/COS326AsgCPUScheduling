90630e0da953648049fb8fb65b2c4989368525fe
Hunt <hunt@covenant.edu>
2016-09-22 13:23:20 -0400

    starting point for asg

---
+++ CPUSim.java
@@ -0,0 +1,13 @@
+/*
+Author: 
+Write a Java program called CPUSim that will read a list of processes.  
+It will simulate the behavior of the processes and then print a number 
+of statistics.  A variety of scheduling algorithms will be implemented.  
+All time are in simulated milliseconds To run the program enter:
+
+Java CPUSim <process file name> <algorithm> <runtime>
+*/
+public class CPUSim {
+    public static void main(String args[]) {
+    }
+}
